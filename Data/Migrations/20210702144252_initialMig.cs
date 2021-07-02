8f2b37798e5c33db2433bc113af9c45b1a916e7b
OnurSaatcioglu <onursaatcioglu@hotmail.com>
2021-07-02 17:44:45 +0300

    migration sonrası

---
+++ Data/Migrations/20210702144252_initialMig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PublicApiExercise.Data.Migrations
+{
+    public partial class initialMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Deneme",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Deger = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deneme", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Deneme");
+        }
+    }
+}
