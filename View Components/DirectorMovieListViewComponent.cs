95e2d015900d069a43d5889c7e49cb019748f84b
OnurSaatcioglu <onursaatcioglu@hotmail.com>
2021-07-05 06:23:42 +0300

    Oyuncuların filmlerinin isimleri viewcomponent ile listelenebilir hale geldi

--- View Components/DirectorMovieListViewComponent.cs
+++ View Components/DirectorMovieListViewComponent.cs
@@ -12,23 +12,49 @@ namespace PublicApiExercise.View_Components
     public class DirectorMovieListViewComponent : ViewComponent
     {
 
-        public ApplicationDbContext _dbcontext { get; }
+        public ApplicationDbContext _context { get; }
 
         public DirectorMovieListViewComponent(ApplicationDbContext dbcontext)
         {
-            _dbcontext = dbcontext;
+            _context = dbcontext;
         }
 
         public async Task<IViewComponentResult> InvokeAsync(int person)
         {
+            var films = _context.Actor.AsQueryable().Where(t => t.TmdbPersonNo == person).Select(p => p.TmdbMuviNo);
 
-            var items = await _dbcontext.Director.AsQueryable().Where(p => p.TmdbPersonNo == person).ToListAsync();
-            
-            if(items.Count == 0)
+            if (films == null)
             {
                 return Content("Yok");
             }
-            return View(items);
+            
+            List<Muvi> muvis = new List<Muvi>();
+
+            foreach (int item in films)
+            {
+
+                Muvi muv = _context.Muvi.FirstOrDefault(t => t.TmdbMuviNo == item);
+
+                if(muv != null)
+                {
+                    muvis.Add(muv);
+                }
+
+            }
+
+            if (muvis.Count == 0)
+            {
+                return Content("Yok");
+            }
+
+            return View(muvis);
+            /*var items = await _context.Director.AsQueryable().Where(p => p.TmdbPersonNo == person).ToListAsync();
+
+            if (items.Count == 0)
+            {
+                return Content("Yok");
+            }
+            return View(items);*/
 
         }
     }
