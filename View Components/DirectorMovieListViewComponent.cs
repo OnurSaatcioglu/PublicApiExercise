96aeea25f26ee81a039b1e7eee9d68c6c203bf3c
OnurSaatcioglu <onursaatcioglu@hotmail.com>
2021-07-05 05:44:47 +0300

    Aynı sayfada birden fazla component denendi ve also bazı buglar çözüldü

---
+++ View Components/DirectorMovieListViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PublicApiExercise.Data;
+using PublicApiExercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PublicApiExercise.View_Components
+{
+    public class DirectorMovieListViewComponent : ViewComponent
+    {
+
+        public ApplicationDbContext _dbcontext { get; }
+
+        public DirectorMovieListViewComponent(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int person)
+        {
+
+            var items = await _dbcontext.Director.AsQueryable().Where(p => p.TmdbPersonNo == person).ToListAsync();
+            
+            if(items.Count == 0)
+            {
+                return Content("Yok");
+            }
+            return View(items);
+
+        }
+    }
+}
